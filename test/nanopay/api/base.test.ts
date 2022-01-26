fe207e639786f46545de23f3d0638b0c1d195245
libitx <libitx@protonmail.com>
2022-01-26 23:04:36 +0000

    Implements tests for MAPI client

--- test/nanopay/api/base.test.ts
+++
@@ -1,4 +0,0 @@
-import test from 'ava'
-
-test.todo('get(path) makes a GET request')
-test.todo('post(path, data) makes a POST request')
--- test/nanopay/mapi.test.ts
+++ test/nanopay/mapi.test.ts
@@ -1,6 +1,56 @@
 import test from 'ava'
+import nock from 'nock'
+import Nanopay from '../../src/index'
 
-test.todo('getTx(txid) returns a payload')
-test.todo('pushTx(rawtx) returns a payload')
-test.todo('pushTx(rawtx[]) returns a payload')
-test.todo('setApi(baseUrl, headers) reconfigures the mapi payload')
+// load fixtures
+import test1 from '../fixtures/mapi.gettx.json'
+import test2 from '../fixtures/mapi.pushtx.single.json'
+import test3 from '../fixtures/mapi.pushtx.multi.json'
+
+test('getTx(txid) returns a payload', async (t) => {
+  const txid = '6886cd04977d4cd26df3689b2d3c40b13685edb41fcc21c2962c5fc64560acff'
+  const scope = nock('https://mapi.taal.com/mapi')
+    .get(`/tx/${ txid }`)
+    .reply(200, test1)
+
+  const nanopay = Nanopay()
+  const payload = await nanopay.mapi.getTx(txid)
+
+  t.is(payload.apiVersion, '1.4.0')
+  t.is(payload.txid, txid)
+  scope.done()
+})
+
+test('pushTx(rawtx) returns a payload', async (t) => {
+  const scope = nock('https://mapi.taal.com/mapi')
+    .post(`/tx`)
+    .reply(200, test2)
+
+  const nanopay = Nanopay()
+  const payload = await nanopay.mapi.pushTx('01000000')
+
+  t.is(payload.apiVersion, '1.4.0')
+  t.is(payload.txid, 'fed22f5ab54202e2ec39cb745d427fcfff960254cde0cf283493ac545f5737f6')
+  scope.done()
+})
+
+test('pushTx(rawtx[]) returns a payload', async (t) => {
+  const scope = nock('https://mapi.taal.com/mapi')
+    .post(`/txs`)
+    .reply(200, test3)
+
+  const nanopay = Nanopay()
+  const payload = await nanopay.mapi.pushTx(['01000000', '01000000'])
+
+  t.is(payload.apiVersion, '1.4.0')
+  t.is(payload.txs!.length, 2)
+  scope.done()
+})
+
+test('setApi(baseUrl, headers) reconfigures the mapi payload', (t) => {
+  const nanopay = Nanopay()
+  nanopay.mapi.setApi('http://localhost', {'x-foo': 'bar'})
+
+  t.is(nanopay.mapi['_api'].baseUrl, 'http://localhost')
+  t.deepEqual(nanopay.mapi['_api'].headers, {'x-foo': 'bar'})
+})
